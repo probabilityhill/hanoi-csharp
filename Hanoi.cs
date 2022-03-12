8e0434f42c8086339b91e41f9244cced46680a1e
okadanao <74280232+probabilityhill@users.noreply.github.com>
2022-03-12 17:35:38 +0900

    initial commit

---
+++ Hanoi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Hanoi {
+  public void Move(int n, char from, char to, char other) {
+    if(n >= 1) {
+      Move(n - 1, from, other, to);
+      Console.WriteLine("move disk {0} from {1} to {2}.", n, from, to);
+      Move(n - 1, other, to, from);
+    }
+  }
+}
+
+class HanoiMain {
+  public static void Main(string[] args) {
+    Hanoi hanoi = new Hanoi();
+
+    Console.Write("How many disks? ");
+    int n = int.Parse(Console.ReadLine());
+
+    // n枚のdiskをAからCへ移す
+    hanoi.Move(n, 'A', 'C', 'B');
+  }
+}
